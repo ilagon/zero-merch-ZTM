2037c1034705f9cf46026042e44e6eb651884c33
Lorenzo <lorenzyadrian@gmail.com>
2021-01-28 23:02:45 +0800

    Implemented Sign up with Email and Password using firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import './App.css';
-import {Route, Switch} from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
   unsubscribeFromAuth = null;
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
 
-  render(){
+  render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path="/shop" component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUp}/>
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUp} />
         </Switch>
       </div>
     );
